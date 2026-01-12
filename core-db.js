281f931eed8d57228edec1d2ffa7ef9afe01e555
universalkunci <universalkunci@gmail.com>
2026-01-12 22:37:28 +0700

    Add files via upload

--- core-db.js
+++ core-db.js
@@ -1,26 +1,27 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { 
-    getFirestore, collection, addDoc, getDocs, setDoc, doc, deleteDoc, query, orderBy 
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+const CoreDB = {
+    dbName: "UniversalGroupDB",
+    dbVersion: 4, // Naik ke versi 4
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyA4ChRlnwmYxo63hVXQUngUzcGyNQWAvP0",
-    authDomain: "universal-group-app.firebaseapp.com",
-    projectId: "universal-group-app",
-    storageBucket: "universal-group-app.firebasestorage.app",
-    messagingSenderId: "541279253826",
-    appId: "1:541279253826:web:79b009baf88da9cf4e44db"
-  };
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export const CloudDB = {
-    async set(col, id, data) { return await setDoc(doc(db, col, id), data, { merge: true }); },
-    async add(col, data) { return await addDoc(collection(db, col), data); },
-    async getAll(col) {
-        const snap = await getDocs(collection(db, col));
-        return snap.docs.map(d => ({ id: d.id, ...d.data() }));
-    },
-    async delete(col, id) { return await deleteDoc(doc(db, col, id)); }
+    init() {
+        return new Promise((resolve, reject) => {
+            const request = indexedDB.open(this.dbName, this.dbVersion);
+            request.onupgradeneeded = (e) => {
+                const db = e.target.result;
+                // Tabel Master Produk
+                if (!db.objectStoreNames.contains("master")) {
+                    db.createObjectStore("master", { keyPath: "sku" });
+                }
+                // Tabel Histori Stok (Kartu Stok)
+                if (!db.objectStoreNames.contains("stock_history")) {
+                    db.createObjectStore("stock_history", { keyPath: "id", autoIncrement: true });
+                }
+                // Tabel Rekap Penjualan
+                if (!db.objectStoreNames.contains("history")) {
+                    db.createObjectStore("history", { keyPath: "id", autoIncrement: true });
+                }
+            };
+            request.onsuccess = (e) => resolve(e.target.result);
+            request.onerror = (e) => reject(e.target.result);
+        });
+    }
 };
